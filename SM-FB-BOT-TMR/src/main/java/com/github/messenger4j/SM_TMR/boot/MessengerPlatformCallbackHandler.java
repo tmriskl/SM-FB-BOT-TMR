eddb83331bdc40a1214500e73fde3cc46cfda33d
Tomer <tmriskl1@gmail.com>
2019-04-14 14:25:52 +0300

    Initial2

--- SM-FB-BOT-TMR/src/main/java/com/github/messenger4j/SM_TMR/boot/MessengerPlatformCallbackHandler.java
+++ SM-FB-BOT-TMR/src/main/java/com/github/messenger4j/SM_TMR/boot/MessengerPlatformCallbackHandler.java
@@ -227,7 +227,7 @@ public class MessengerPlatformCallbackHandler {
                     break;
 
                 default:
-                    sendTextMessage(senderId, messageText);
+                    sendTextMessage(senderId, messageText+" "+messageText);
             }
         } catch (MessengerApiException | MessengerIOException | MalformedURLException e) {
             handleSendException(e);
